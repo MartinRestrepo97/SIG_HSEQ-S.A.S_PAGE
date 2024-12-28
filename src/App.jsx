67138102aba78678b68428e66622a0342884b040
MartinRestrepo97 <palaciosrestrepomartinernesto@gmail.com>
2024-12-27 22:04:51 -0500

    sig_hseq_sas_front_2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,48 @@
-export default function App() {
+// import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  /* const [email, setEmail] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Gracias por suscribirte con el correo: ${email}`);
+  }; */
+
   return (
-    <>
-      <h1>App</h1>
-    </>
+    <div className="App">
+      <body>
+        <header>
+          <h1>Landing Page - Consulta de Certificados</h1>
+        </header>
+
+        <main>
+          <h2>Ingresa tu correo para consultar tus certificados</h2>
+          <form action="#" method="POST">
+            <label >Correo electrónico</label>
+            <input 
+              type="email" 
+              id="email" 
+              name="email" 
+              placeholder="ejemplo@correo.com" 
+              required 
+            />
+
+            <button type="submit">Consultar</button>
+          </form>
+
+          
+          <section id="resultados">
+            
+          </section>
+        </main>
+
+        <footer>
+          <p>© 2024 Consulta de Certificados. Todos los derechos reservados.</p>
+        </footer>
+      </body>
+    </div>
   );
 }
+
+export default App;
