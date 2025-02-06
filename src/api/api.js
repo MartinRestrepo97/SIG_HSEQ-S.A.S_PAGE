40359568ab54ab97acd2a99f93a3023fd145d956
MartinRestrepo97 <palaciosrestrepomartinernesto@gmail.com>
2025-02-05 23:42:34 -0500

    change server dir

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000', // URL de tu backend Laravel
+    baseURL: 'http://52.14.137.11', // URL de tu backend Laravel
     headers: {
         'Content-Type': 'application/json',
     },
