ad41e5cfea0472f8e533e7cc99d7609c393ee194
MartinRestrepo97 <palaciosrestrepomartinernesto@gmail.com>
2025-02-06 00:21:29 -0500

    Add api to back url

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://52.14.137.11', // URL de tu backend Laravel
+    baseURL: 'http://52.14.137.11/api', // URL de tu backend Laravel
     headers: {
         'Content-Type': 'application/json',
     },
